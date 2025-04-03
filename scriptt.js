bb8080bf8f26a8c549aa0796dd2dfe3ea0efed72
Li-hype <livia.m.rodrigues@aluno.senai.br>
2025-04-03 07:31:44 -0300

    exercicio 2 dom

--- scriptt.js
+++ scriptt.js
@@ -28,3 +28,27 @@ console.log("Exibindo com Query Selector" + esportesQS[2]);
 
 let esportesQS3 = document.querySelector(".esp");
 console.log("Exibindo com Query Selector" + esportesQS[3]);
+
+titulo.innerText = "Novo Título para o nosso H1";
+titulo.innerHTML = "<span style='color:rgb(37, 37, 189)'> MEUS ESPORTES FAVORITOS </span>";
+
+/* Pegamos a nossa UL*/
+let lista = document.getElementById("lista-esporte");
+
+
+let novoLi = document.createElement("li"); 
+novoLi.innerText = "Natação";
+novoLi.style.color = "rgb(37, 37, 189)";
+novoLi.style.fontWeight = "bold";
+
+let novoLi1 = document.createElement("li");
+novoLi1.innerText = "Hipismo";
+novoLi1.style.color = "rgb(37, 37, 189)";
+novoLi1.style.fontWeight = "bold";
+
+
+//Adicionando o novo parágrafo ao body
+lista.appendChild(novoLi);
+lista.appendChild(novoLi1);
+
+
